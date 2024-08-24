fffa8b8e02b66b2ef25a1eb6c7f68f6a9b4042ea
tsusonobe <sonobe.tsu@gmail.com>
2024-08-23 22:34:46 -0300

    adicionando player que toca a musica mais ouvida do usuário

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -45,6 +45,7 @@ export default function Dashboard(props) {
 		if(accessToken){
 			const getTopItems = async () => {
 				const items = await getTop(accessToken, type, timeRange, limit);
+				console.log(items);
 				if (type === "tracks") {
 					setTracks(
 						items.map((track) => {
@@ -81,6 +82,22 @@ export default function Dashboard(props) {
 
 	return (
 		<>
+			{(!isLoading && tracks) && 
+				<div className="player-container">
+					<iframe style={{borderRadius:12 + "px", border: 0}} src={`https://open.spotify.com/embed/track/${tracks[0].id}?utm_source=generator&theme=0`} width="100%" height="152" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+				</div>
+			}
+			{(isLoading || !tracks) && 
+				<div className="player-container">
+					<div className="loading-player">
+						<div className="loading-img-player"/>
+						<div className="loading-track-player"/>
+						<div className="loading-artist-player"/>
+					</div>
+				</div>
+			}
+		
+
 			<div className="grid-container">
 				<aside>
 					<div className="window-controls">
